83990597897741c8917fa31efd6afcc99b9c0000
haoun1 <85067758+haoun1@users.noreply.github.com>
2021-10-04 14:59:02 +0900

    빨간색이 깜빡
    1

--- siren.py
+++ siren.py
@@ -12,12 +12,12 @@ def call_siren():
     end_point = 0x01
     buffer[0] = 0x57  # Write mode
     buffer[1] = 0x00
-    buffer[2]= 0 # LED: 0-Off, 1-On, 2-Blink
+    buffer[2]= 2 # LED: 0-Off, 1-On, 2-Blink
     buffer[3] =0
     buffer[4] = 0
     buffer[5] = 0x00
     buffer[6] = 0x00
-    buffer[7]= 0 # Sound: 0-Off, 1~4-On
+    buffer[7]= 1 # Sound: 0-Off, 1~4-On
 
     # write the data
     rtn = dev.write(end_point, buffer)
